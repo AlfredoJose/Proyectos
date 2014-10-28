9d32fdf256ad2e00ca7d58bac5701ffed8da1d37
Alfredo <alfredobertho@hotmail.com>
2014-10-28 18:55:14 -0430

    Practicas2

---
+++ alfredo/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Alfredo - Tutos</title>
+	<link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Allura' rel='stylesheet' type='text/css'>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script src="assets/js/smoothscroll.js" type="text/javascript"></script>
+	<script src="assets/js/appear.min.js" type="text/javascript"></script>
+	<script src="assets/js/animations.min.js" type="text/javascript"></script>
+	<script src="assets/js/backbone.js" type="text/javascript"></script>
+	<script type="text/javascript" src="assets/js/animations.js"></script>
+	<script type="text/javascript" src="assets/js/animations.min.js"></script>
+	<script type="text/javascript" src="assets/js/appear.js"></script>
+	<script type="text/javascript" src="assets/js/backbone.js"></script>
+	<script type="text/javascript" src="assets/js/smoothscroll.min.js"></script>
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<link rel="stylesheet" href="assets/css/animations.min.css"/>
+</head>
+<body>
+	<section class="section1">
+		<div class="article1">
+			<h1 class="titulo1 animate-in" data-anim-type="fade-in-up" data-anim-delay="10">Alfredo Jose</h1>
+			<img class="animate-in" data-anim-type="fade-in-down" data-anim-delay="10" src="img/desk.png" alt="">
+		</div>
+		<div class="article2">
+			<h1 class="titulo2">¿ Quien es Alfredo Jose ?</h1>
+		</div>
+	</section>
+
+	<section class="section2">
+		<div class="article3">
+			<h1 class="titulo3 animate-in" data-anim-type="fade-in" data-anim-delay="10" >¿ Quien es Alfredo Jose ?</h1>
+			<p class="parrafo1">Es un joven de 15 años apasionado por la programacion y el diseño web, que te enseñara a programar en varios lenguajes de programacion.</p>
+		</div>
+		<div class="article4 animate-in" data-anim-type="fade-in-down" data-anim-delay="0">
+			<h1 class="titulo4">Android</h1>
+			<img src="img/a.png" class="a" alt="">
+			<p class="parrafo2">Android es un sistema operativo basado en el kernel de Linux diseñado principalmente para dispositivos móviles con pantalla táctil, como teléfonos inteligentes o tabletas, y también para relojes inteligentes, televisores y automóviles, inicialmente desarrollado por Android Inc.</p>
+		</div>
+		<div class="article5 animate-in" data-anim-type="fade-in-right" data-anim-delay="100">
+			<h1 class="titulo4">PHP</h1>
+			<img src="img/php.png" class="a" alt="">
+			<p class="parrafo2">PHP es un lenguaje de programación de uso general de código del lado del servidor originalmente diseñado para el desarrollo web de contenido dinámico.Fue uno de los primeros lenguajes de programación del lado del servidor que se podían incorporar directamente en el documento HTML</p>
+		</div>
+		<div class="article6 animate-in" data-anim-type="fade-in-left" data-anim-delay="200">
+			<h1 class="titulo4">Javascript</h1>
+			<img src="img/js.png" class="a" alt="">
+			<p class="parrafo4">JavaScript (abreviado comúnmente "JS") es un lenguaje de programación interpretado, dialecto del estándar ECMAScript. Se define como orientado a objetos,3 basado en prototipos, imperativo, débilmente tipado y dinámico.</p>
+		</div>
+		<div class="article7 animate-in" data-anim-type="fade-in-up" data-anim-delay="300">
+			<h1 class="titulo7">python</h1>
+			<img src="img/py.png" class="a7" alt="">
+			<p class="parrafo5">Python es un lenguaje de programación interpretado cuya filosofía hace hincapié en una sintaxis que favorezca un código legible.Es administrado por la Python Software Foundation,es compatible con la Licencia pública general de GNU.</p>
+		</div>
+		<div class="article8">
+			<h1 class="titulo8">¿ Quieres una pagina web ?</h1>
+		</div>
+	</section>
+	<section class="section3">
+		<div class="article9">
+			<h1 class="titulo9 animate-in" data-anim-type="fade-in-right" data-anim-delay="300">Contactenos</h1>
+			<img src="img/contacto.png" data-anim-type="zoom-in" data-anim-delay="300" class="a2 animate-in" alt="">
+		</div>
+		<div class="article10">
+			<a href="https://www.facebook.com/pages/OAPP/716336118389517"><img src="img/facebook.png" data-anim-type="fade-in-left-big" data-anim-delay="0" class="red animate-in" alt=""></a>
+			<a href=""><img src="img/tw.png" data-anim-type="fade-in-left-big" data-anim-delay="200"  class="red animate-in" alt=""></a>
+			<a href="https://www.youtube.com/channel/UCMnmwFkWtYQ9jvKrkNTxbrA"><img src="img/yt.png" data-anim-type="fade-in-left-big" data-anim-delay="300" class="red animate-in" alt=""></a>
+		</div>
+	</section>
+</body>
+</html>
